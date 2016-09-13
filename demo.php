0a25dc306fd5d2e017bcedb0675a8c0e954b04d7
Jorge Roberto Neyra <roneypc@gmail.com>
2016-09-13 18:12:08 +0200

    Commit demo

---
+++ demo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$e = 17;
+if ($e > 0): ?>
+    Número positivo.
+<?php elseif($e < 0): ?>
+	Número negativo
+<?php else: ?>
+	Es CERO..!
+<?php endif ?>
+
+</body>
+</html>
+
